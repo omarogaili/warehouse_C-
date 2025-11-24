80563242d6fcfb0062984f1fcef5dd13932d78d6
omar ogaili <ogialiomar@hotmail.com>
2025-11-24 23:36:57 +0100

    feat/ created user models as required.

--- obj/Debug/net8.0/warehouse.AssemblyInfo.cs
+++ obj/Debug/net8.0/warehouse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("warehouse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10b2b6437e946e3d13033045b12c99a036d83031")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec98b0d635a9dd33937be823bc9d537352d63ae3")]
 [assembly: System.Reflection.AssemblyProductAttribute("warehouse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("warehouse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
